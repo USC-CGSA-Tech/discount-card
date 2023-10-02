e4390441fbcb77542f4d1b66a94c6aacb4f65d42
O0OMichael <754105906@qq.com>
2023-10-01 17:01:01 -0700

    business.sql

---
+++ backend/business.sql
@@ -0,0 +1,5 @@
+INSERT INTO `business` VALUES (1, 'asdasd', '123123123', '123das12', '123123@mail.com', '123123123', '123123123', 'asdasd', 'http://dasdasd', NULL, 0, '2023-10-01 14:01:31');
+INSERT INTO `business` VALUES (2, 'test', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-10-01 15:26:18', NULL, '2023-10-01 15:26:18');
+INSERT INTO `business` VALUES (3, 'test3', NULL, NULL, '111@email.com', NULL, NULL, NULL, NULL, '2023-10-01 15:41:05', 0, '2023-10-01 16:16:15');
+INSERT INTO `business` VALUES (4, 'test4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-10-01 15:41:17', 0, '2023-10-01 15:41:17');
+INSERT INTO `business` VALUES (5, 'test4', '123123123', 'asddas123123', 'asdasdasd', 'asdasdsad', 'asdasdasd', NULL, NULL, '2023-10-01 15:41:41', 1, '2023-10-01 16:06:19');
