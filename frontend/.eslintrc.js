4c0832d56b97a6b728f68a83a5694a62209485e5
Haozhe Zhang <zhz19980514@gmail.com>
2023-11-08 23:04:26 -0600

    chore: reworked ts prettier and eslint config files

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,67 +1,62 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
+    es2021: true,
     node: true,
-    es6: true,
   },
-  parserOptions: {
-    ecmaVersion: 8,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-  },
-  ignorePatterns: ['dist', 'node_modules/*'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint'],
   extends: [
-    'next/core-web-vitals',
-    'react',
-    'plugin:@typescript-eslint/recommended',
     'airbnb',
-    'prettier',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'plugin:security/recommended',
-  ],
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      settings: {
-        react: {
-          version: 'detect',
-        },
-      },
-      extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
-      rules: {
-        'no-console': 'error',
-        '@typescript-eslint/no-unused-vars': 'error',
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-        'import/extensions': [
-          'error',
-          'ignorePackages',
-          {
-            js: 'never',
-            jsx: 'never',
-            ts: 'never',
-            tsx: 'never',
-          },
-        ],
-        'prettier/prettier': [
-          'error',
-          {
-            endOfLine: 'auto',
-          },
-        ],
-      },
-    },
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/react',
   ],
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+      alias: [['src', './src']],
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'unicorn'],
+  rules: {
+    semi: 0,
+    indent: 0,
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+    'no-use-before-define': 0,
+    'no-unused-vars': 0,
+    'implicit-arrow-linebreak': 0,
+    'consistent-return': 0,
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'operator-linebreak': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', '.'], // 将 '@' 替换为实际别名，路径替换为实际的路径
+        ],
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+      },
     },
   },
 };
